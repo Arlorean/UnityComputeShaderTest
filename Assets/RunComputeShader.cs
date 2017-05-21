dc80d006ace4d376c85b05c9bc8f8a69dce27402
Adam Davidson <adam@adam-davidson.com>
2017-05-21 19:01:58 +0100

    Initial commit
    Unity scene with full screen RawImage that displays a RenderTexture populated using a Compute Shader.

---
+++ Assets/RunComputeShader.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RunComputeShader : MonoBehaviour {
+    public ComputeShader shader;
+    RenderTexture texture;
+    int numThreadsGroupsX, numThreadsGroupsY, numThreadsGroupsZ = 1;
+
+    void Start() {
+        UpdateTextureSize();
+    }
+
+    void UpdateTextureSize() {
+        var width = Screen.width;
+        var height = Screen.height;
+        if (texture != null && texture.width == width && texture.height == height) {
+            return;
+        }
+        if (texture != null) {
+            texture.Release();
+        }
+
+        texture = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+        texture.filterMode = FilterMode.Point;
+        texture.enableRandomWrite = true;
+        texture.Create();
+
+        var rawImage = GetComponent<RawImage>();
+        rawImage.rectTransform.anchorMin = Vector2.zero;
+        rawImage.rectTransform.anchorMax = Vector2.one;
+        rawImage.rectTransform.sizeDelta = Vector2.zero;
+        rawImage.texture = texture;
+
+        uint numThreadsX, numThreadsY, numThreadsZ;
+        shader.GetKernelThreadGroupSizes(0, out numThreadsX, out numThreadsY, out numThreadsZ);
+        numThreadsGroupsX = (int)(texture.width / numThreadsX);
+        numThreadsGroupsY = (int)(texture.height / numThreadsY);
+
+        shader.SetTexture(0, "Result", texture);
+        shader.SetVector("iResolution", new Vector4(width, height));
+    }
+
+    private void Update() {
+        UpdateTextureSize();
+
+        shader.SetFloat("iGlobalTime", Time.time);
+        shader.Dispatch(0, numThreadsGroupsX, numThreadsGroupsY, numThreadsGroupsZ);
+    }
+}
